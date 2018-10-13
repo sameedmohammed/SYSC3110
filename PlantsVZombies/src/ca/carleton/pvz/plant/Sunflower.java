665e343594572f0da16078532dbfc0f19bef76b4
Jacob Laboissonniere <jacob.laboissonniere@carleton.ca>
2018-10-13 16:43:38 -0400

    Created a number of starter classes

---
+++ PlantsVZombies/src/ca/carleton/pvz/plant/Sunflower.java
@@ -0,0 +1,12 @@
+package ca.carleton.pvz.plant;
+
+import java.awt.Point;
+
+public class Sunflower extends Plant {
+
+	public Sunflower(int cost, Point position) {
+		super(cost, position);
+
+	}
+	
+}
