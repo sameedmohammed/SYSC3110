705ad5810ed706c7fb9a0499817aa56161516b82
Jacob Laboissonniere <jacob.laboissonniere@carleton.ca>
2018-10-17 17:03:53 -0400

    Added zombie wave class

---
+++ PlantsVZombies/src/ca/carleton/pvz/zombie/Wave.java
@@ -0,0 +1,21 @@
+package ca.carleton.pvz.zombie;
+
+public class Wave {
+	private Zombie[] zombies;
+	private int numZombies;
+	private int remainingZombies;
+	
+	public Wave(int numZombies) {
+		zombies = new Zombie[numZombies];
+		this.numZombies = numZombies;
+		this.remainingZombies = numZombies;
+		
+		for(int i = 0; i < numZombies; i++) {
+			zombies[i] = new Zombie();
+		}
+	}
+	
+	public int getRemainingZombies() {
+		return remainingZombies;
+	}
+}
