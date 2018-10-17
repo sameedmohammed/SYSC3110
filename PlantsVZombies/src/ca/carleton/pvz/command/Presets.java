23cdd12e883fb30a430256d8ed49d7736fabbb7c
Jacob Laboissonniere <jacob.laboissonniere@carleton.ca>
2018-10-16 20:34:56 -0400

    Command parsing and text-output work.

---
+++ PlantsVZombies/src/ca/carleton/pvz/command/Presets.java
@@ -0,0 +1,10 @@
+package ca.carleton.pvz.command;
+
+public final class Presets {
+	public static final String WELCOME = "Welcome to Plants VS Zombies (text-based)! \n"
+										 + "Type 'help' if you need help. \n";
+	
+	public static final String INVALID = "You entered an invalid command. Type 'help' for valid commands. \n";
+	
+	public static final String HELP = "Your valid command words are: \n" + Parser.getCommands();
+}
