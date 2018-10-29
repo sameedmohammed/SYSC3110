fadcb8e84cf8e5303014861712c449ebb5d019f0
Jacob Laboissonniere <jacob.laboissonniere@carleton.ca>
2018-10-28 20:57:26 -0400

    Added superclass Actor

--- PlantsVZombies/src/ca/carleton/pvz/actor/Actor.java
+++ PlantsVZombies/src/ca/carleton/pvz/actor/Actor.java
@@ -1,8 +1,10 @@
-package ca.carleton.pvz.plant;
+package ca.carleton.pvz.actor;
 
 /**
  * The parent class from which different plant species inherit.
  * (Not yet implemented, but leaving here for future iterations.)
  *
  */
-public class Plant {}
+public class Actor {
+	
+}
